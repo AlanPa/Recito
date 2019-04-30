a96a27e5dab0d109756b21ce188d590686433fd1
Clemenking <misshermione19@live.fr>
2019-04-30 09:18:56 +0200

    squelette application android

---
+++ app/src/main/java/samples/speech/cognitiveservices/microsoft/com/MainActivity.java
@@ -0,0 +1,71 @@
+package samples.speech.cognitiveservices.microsoft.com;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.microsoft.cognitiveservices.speech.ResultReason;
+import com.microsoft.cognitiveservices.speech.SpeechConfig;
+import com.microsoft.cognitiveservices.speech.SpeechRecognitionResult;
+import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
+
+import java.util.concurrent.Future;
+
+import static android.Manifest.permission.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Replace below with your own subscription key
+    private static String speechSubscriptionKey = "5eae85560bb241b884f09a170d1a3214";
+    // Replace below with your own service region (e.g., "westus").
+    private static String serviceRegion = "westus";
+
+    // Includes code that requests microphone and internet permissions, and initializes the native platform binding
+    // Configuring the native platform bindings is only required once. It should be done early during application initialization.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Note: we need to request the permissions
+        int requestCode = 5; // unique code for the permission request
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, INTERNET}, requestCode);
+    }
+
+    // The method onSpeechButtonClicked is, as noted earlier, the button click handler.
+    // A button press triggers speech to text transcription.
+    public void onSpeechButtonClicked(View v) {
+        TextView txt = (TextView) this.findViewById(R.id.hello); // 'hello' is the ID of your text view
+
+        try {
+            SpeechConfig config = SpeechConfig.fromSubscription(speechSubscriptionKey, serviceRegion);
+            assert(config != null);
+
+            SpeechRecognizer reco = new SpeechRecognizer(config);
+            assert(reco != null);
+
+            Future<SpeechRecognitionResult> task = reco.recognizeOnceAsync();
+            assert(task != null);
+
+            // Note: this will block the UI thread, so eventually, you want to
+            //        register for the event (see full samples)
+            SpeechRecognitionResult result = task.get();
+            assert(result != null);
+
+            if (result.getReason() == ResultReason.RecognizedSpeech) {
+                txt.setText(result.toString());
+            }
+            else {
+                txt.setText("Error recognizing. Did you update the subscription info?" + System.lineSeparator() + result.toString());
+            }
+
+            reco.close();
+        } catch (Exception ex) {
+            Log.e("SpeechSDKDemo", "unexpected " + ex.getMessage());
+            assert(false);
+        }
+    }
+}
